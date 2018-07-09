fb7d08a98e0f182a9fd5243f45fde16fb4c3fbf9
LyleCheatham <hello@lylecheatham.com>
2018-07-09 17:34:23 -0400

    Revert the src/ext files to be stock, seems that there was an issue

--- src/ext/RTX_config.c
+++ src/ext/RTX_config.c
@@ -24,7 +24,7 @@
 //   <i> Define max. number of tasks that will run at the same time.
 //   <i> Default: 6
 #ifndef OS_TASKCNT
-#define OS_TASKCNT 6
+ #define OS_TASKCNT     6
 #endif
 
 //   <o>Number of tasks with user-provided stack <0-250>
@@ -32,14 +32,14 @@
 //   <i> The memory space for the stack is provided by the user.
 //   <i> Default: 0
 #ifndef OS_PRIVCNT
-#define OS_PRIVCNT 0
+ #define OS_PRIVCNT     0
 #endif
 
 //   <o>Task stack size [bytes] <20-4096:8><#/4>
 //   <i> Set the stack size for tasks which is assigned by the system.
 //   <i> Default: 512
 #ifndef OS_STKSIZE
-#define OS_STKSIZE 128
+ #define OS_STKSIZE     128
 #endif
 
 // <q>Check for the stack overflow
@@ -47,7 +47,7 @@
 // <i> Include the stack checking code for a stack overflow.
 // <i> Note that additional code reduces the Kernel performance.
 #ifndef OS_STKCHECK
-#define OS_STKCHECK 1
+ #define OS_STKCHECK    1
 #endif
 
 // <q>Run in privileged mode
@@ -55,7 +55,7 @@
 // <i> Run all Tasks in privileged mode.
 // <i> Default: Unprivileged
 #ifndef OS_RUNPRIV
-#define OS_RUNPRIV 0
+ #define OS_RUNPRIV     0
 #endif
 
 // </h>
@@ -65,21 +65,21 @@
 //   <i> Define the on-chip timer used as a time-base for RTX.
 //   <i> Default: Core SysTick
 #ifndef OS_TIMER
-#define OS_TIMER 0
+ #define OS_TIMER       0
 #endif
 
 //   <o>Timer clock value [Hz] <1-1000000000>
 //   <i> Set the timer clock value for selected timer.
 //   <i> Default: 6000000  (6MHz)
 #ifndef OS_CLOCK
-#define OS_CLOCK 60000000
+ #define OS_CLOCK       60000000
 #endif
 
 //   <o>Timer tick value [us] <1-1000000>
 //   <i> Set the timer tick value for selected timer.
 //   <i> Default: 10000  (10ms)
 #ifndef OS_TICK
-#define OS_TICK 10000
+ #define OS_TICK        10000
 #endif
 
 // </h>
@@ -90,14 +90,14 @@
 // =============================
 // <i> Enable Round-Robin Task switching.
 #ifndef OS_ROBIN
-#define OS_ROBIN 1
+ #define OS_ROBIN       1
 #endif
 
 //   <o>Round-Robin Timeout [ticks] <1-1000>
 //   <i> Define how long a task will execute before a task switch.
 //   <i> Default: 5
 #ifndef OS_ROBINTOUT
-#define OS_ROBINTOUT 5
+ #define OS_ROBINTOUT   5
 #endif
 
 // </e>
@@ -106,7 +106,7 @@
 //   <i> Define max. number of user timers that will run at the same time.
 //   <i> Default: 0  (User timers disabled)
 #ifndef OS_TIMERCNT
-#define OS_TIMERCNT 0
+ #define OS_TIMERCNT    0
 #endif
 
 //   <o>ISR FIFO Queue size<4=>   4 entries  <8=>   8 entries
@@ -118,7 +118,7 @@
 //   <i> when they are called from the iterrupt handler.
 //   <i> Default: 16 entries
 #ifndef OS_FIFOSZ
-#define OS_FIFOSZ 16
+ #define OS_FIFOSZ      16
 #endif
 
 // </h>
@@ -127,17 +127,17 @@
 
 // Standard library system mutexes
 // ===============================
-//  Define max. number system mutexes that are used to protect
+//  Define max. number system mutexes that are used to protect 
 //  the arm standard runtime library. For microlib they are not used.
 #ifndef OS_MUTEXCNT
-#define OS_MUTEXCNT 8
+ #define OS_MUTEXCNT    8
 #endif
 
 /*----------------------------------------------------------------------------
  *      RTX User configuration part END
  *---------------------------------------------------------------------------*/
 
-#define OS_TRV ((U32)(((double)OS_CLOCK * (double)OS_TICK) / 1E6) - 1)
+#define OS_TRV          ((U32)(((double)OS_CLOCK*(double)OS_TICK)/1E6)-1)
 
 /*----------------------------------------------------------------------------
  *      Global Functions
@@ -145,53 +145,54 @@
 
 /*--------------------------- os_idle_demon ---------------------------------*/
 
-__task void os_idle_demon(void) {
-    /* The idle demon is a system task, running when no other task is ready */
-    /* to run. The 'os_xxx' function calls are not allowed from this task.  */
+__task void os_idle_demon (void) {
+  /* The idle demon is a system task, running when no other task is ready */
+  /* to run. The 'os_xxx' function calls are not allowed from this task.  */
 
-    for (;;) {
-        /* HERE: include optional user code to be executed when no task runs.*/
-    }
+  for (;;) {
+  /* HERE: include optional user code to be executed when no task runs.*/
+  }
 }
 
 /*--------------------------- os_tick_init ----------------------------------*/
 
 #if (OS_TIMER != 0)
-int os_tick_init(void) {
-    /* Initialize hardware timer as system tick timer. */
-    /* ... */
-    return (-1); /* Return IRQ number of timer (0..239) */
+int os_tick_init (void) {
+  /* Initialize hardware timer as system tick timer. */
+  /* ... */
+  return (-1);  /* Return IRQ number of timer (0..239) */
 }
 #endif
 
 /*--------------------------- os_tick_irqack --------------------------------*/
 
 #if (OS_TIMER != 0)
-void os_tick_irqack(void) {
-    /* Acknowledge timer interrupt. */
-    /* ... */
+void os_tick_irqack (void) {
+  /* Acknowledge timer interrupt. */
+  /* ... */
 }
 #endif
 
 /*--------------------------- os_tmr_call -----------------------------------*/
 
-void os_tmr_call(U16 info) {
-    /* This function is called when the user timer has expired. Parameter   */
-    /* 'info' holds the value, defined when the timer was created.          */
+void os_tmr_call (U16 info) {
+  /* This function is called when the user timer has expired. Parameter   */
+  /* 'info' holds the value, defined when the timer was created.          */
 
-    /* HERE: include optional user code to be executed on timeout. */
+  /* HERE: include optional user code to be executed on timeout. */
 }
+
 
 /*--------------------------- os_error --------------------------------------*/
 
-void os_error(U32 err_code) {
-    /* This function is called when a runtime error is detected. Parameter */
-    /* 'err_code' holds the runtime error code (defined in RTL.H).         */
+void os_error (U32 err_code) {
+  /* This function is called when a runtime error is detected. Parameter */
+  /* 'err_code' holds the runtime error code (defined in RTL.H).         */
 
-    /* HERE: include optional code to be executed on runtime error. */
-    for (;;)
-        ;
+  /* HERE: include optional code to be executed on runtime error. */
+  for (;;);
 }
+
 
 /*----------------------------------------------------------------------------
  *      RTX Configuration Functions
--- src/ext/uart.c
+++ src/ext/uart.c
@@ -3,7 +3,7 @@
  *   Project: NXP LPC17xx UART example
  *
  *   Description:
- *     This file contains UART code example which include UART initialization,
+ *     This file contains UART code example which include UART initialization, 
  *     UART interrupt handler, and APIs for UART access.
  *
  ****************************************************************************
@@ -17,7 +17,7 @@
  * notification. NXP Semiconductors also make no representation or
  * warranty that such application will be suitable for the specified
  * use without further testing or modification.
- ****************************************************************************/
+****************************************************************************/
 #include "lpc17xx.h"
 //#include "type.h"
 #include "uart.h"
@@ -25,7 +25,7 @@
 extern uint32_t SystemCoreClock;
 
 //#ifdef __DBG_ITM
-volatile int ITM_RxBuffer = ITM_RXBUFFER_EMPTY; /*  CMSIS Debug Input        */
+volatile int ITM_RxBuffer = ITM_RXBUFFER_EMPTY;  /*  CMSIS Debug Input        */
 //#endif
 
 volatile uint32_t UART0Status, UART1Status;
@@ -33,45 +33,52 @@ volatile uint8_t UART0TxEmpty = 1, UART1TxEmpty = 1;
 volatile uint8_t UART0Buffer[BUFSIZE], UART1Buffer[BUFSIZE];
 volatile uint32_t UART0Count = 0, UART1Count = 0;
 
-volatile uint8_t RcvLock0;
-volatile uint8_t SndLock0;
+volatile uint8_t RcvLock0; 
+volatile uint8_t SndLock0; 
 
-volatile uint8_t RcvLock1;
-volatile uint8_t SndLock1;
+volatile uint8_t RcvLock1; 
+volatile uint8_t SndLock1; 
 
 volatile int i = 0;
 
-void Free(volatile uint8_t *tbl) { *tbl = 0; }
-
-uint8_t Lock(volatile uint8_t *tbl) {
-    // Get the lock status and see if it is already locked
-    if (__LDREXW(tbl) == 0) {
-        // if not locked, try set lock to 1
-        return (__STREXW(1, tbl) != 0);
-    } else {
-        return (1);  // return fail status
-    }
+void Free(volatile uint8_t *tbl){
+	*tbl = 0;
 }
 
-uint8_t LockRcv(uint8_t portNum) {
-    if (portNum > 1) return 0x1;
-    return Lock(portNum == 0 ? &RcvLock0 : &RcvLock1);
+uint8_t Lock(volatile uint8_t *tbl){
+	// Get the lock status and see if it is already locked
+	if (__LDREXW(tbl) == 0) {
+		// if not locked, try set lock to 1
+		return  (__STREXW(1, tbl) != 0) ;
+	} else {
+		return(1); // return fail status
+	}
 }
 
-uint8_t LockSnd(uint8_t portNum) {
-    if (portNum > 1) return 0x1;
-    return Lock(portNum == 0 ? &SndLock0 : &SndLock1);
+uint8_t LockRcv(uint8_t portNum){
+	if(portNum > 1)
+		return 0x1;
+	return Lock(portNum == 0? &RcvLock0 : &RcvLock1);
 }
 
-void FreeRcv(uint8_t portNum) {
-    if (portNum > 1) return;
-    Free(portNum == 0 ? &RcvLock0 : &RcvLock1);
+uint8_t LockSnd(uint8_t portNum){
+	if(portNum > 1)
+		return 0x1;
+	return Lock(portNum == 0? &SndLock0 : &SndLock1);
 }
 
-void FreeSnd(uint8_t portNum) {
-    if (portNum > 1) return;
-    Free(portNum == 0 ? &SndLock0 : &SndLock1);
+void FreeRcv(uint8_t portNum){
+	if(portNum > 1)
+		return;
+	Free( portNum == 0? &RcvLock0 : &RcvLock1 );
 }
+
+void FreeSnd(uint8_t portNum){
+	if(portNum > 1)
+		return;
+	Free( portNum == 0? &SndLock0 : &SndLock1 );
+}
+
 
 /*****************************************************************************
 ** Function name:		UART0_IRQHandler
@@ -80,40 +87,44 @@ void FreeSnd(uint8_t portNum) {
 **
 ** parameters:			None
 ** Returned value:		None
-**
+** 
 *****************************************************************************/
-void UART0_IRQHandler(void) {
-    uint8_t IIRValue, LSRValue;
+void UART0_IRQHandler (void) 
+{
+	uint8_t IIRValue, LSRValue;
 
-    IIRValue = LPC_UART0->IIR;
+	IIRValue = LPC_UART0->IIR;
 
-    IIRValue >>= 1;   /* skip pending bit in IIR */
-    IIRValue &= 0x07; /* check bit 1~3, interrupt identification */
+	IIRValue >>= 1;			/* skip pending bit in IIR */
+	IIRValue &= 0x07;			/* check bit 1~3, interrupt identification */
 
-    LSRValue = LPC_UART0->LSR;
+	LSRValue = LPC_UART0->LSR;
 
-    if (LSRValue & LSR_RDR) /* Receive Data Ready */
-    {
-        /* If no error on RLS, normal ready, save into the data buffer. */
-        /* Note: read RBR will clear the interrupt */
-        UART0Buffer[UART0Count] = LPC_UART0->RBR;
-        UART0Count++;
-        if (UART0Count == BUFSIZE) {
-            UART0Count = 0; /* buffer overflow */
-        }
-    }
+	if ( LSRValue & LSR_RDR )	/* Receive Data Ready */	
+	{
+		/* If no error on RLS, normal ready, save into the data buffer. */
+		/* Note: read RBR will clear the interrupt */
+		UART0Buffer[UART0Count] = LPC_UART0->RBR;
+		UART0Count++;
+		if ( UART0Count == BUFSIZE )
+		{
+		UART0Count = 0;		/* buffer overflow */
+		}
+	}
 
-    if (IIRValue == IIR_THRE) /* THRE, transmit holding register empty */
-    {
-        /* THRE interrupt */
-        LSRValue = LPC_UART0->LSR; /* Check status in the LSR to see if
-                                                   valid data in U0THR or not */
-        if (LSRValue & LSR_THRE) {
-            UART0TxEmpty = 1;
-        } else {
-            UART0TxEmpty = 0;
-        }
-    }
+	if ( IIRValue == IIR_THRE )	/* THRE, transmit holding register empty */
+	{
+	/* THRE interrupt */
+		LSRValue = LPC_UART0->LSR;		/* Check status in the LSR to see if
+									valid data in U0THR or not */
+		if ( LSRValue & LSR_THRE ){
+			UART0TxEmpty = 1;
+		}
+		else{
+			UART0TxEmpty = 0;
+		}
+	}
+
 }
 
 /*****************************************************************************
@@ -123,142 +134,144 @@ void UART0_IRQHandler(void) {
 **
 ** parameters:			None
 ** Returned value:		None
-**
+** 
 *****************************************************************************/
-void UART1_IRQHandler(void) {
-    uint8_t IIRValue, LSRValue;
+void UART1_IRQHandler (void) 
+{
 
-    IIRValue = LPC_UART1->IIR;
+	uint8_t IIRValue, LSRValue;
 
-    IIRValue >>= 1;   /* skip pending bit in IIR */
-    IIRValue &= 0x07; /* check bit 1~3, interrupt identification */
+	IIRValue = LPC_UART1->IIR;
 
-    LSRValue = LPC_UART1->LSR;
+	IIRValue >>= 1;			/* skip pending bit in IIR */
+	IIRValue &= 0x07;			/* check bit 1~3, interrupt identification */
 
-    if (LSRValue & LSR_RDR) /* Receive Data Ready */
-    {
-        /* If no error on RLS, normal ready, save into the data buffer. */
-        /* Note: read RBR will clear the interrupt */
-        UART1Buffer[UART1Count] = LPC_UART1->RBR;
-        UART1Count++;
-        if (UART1Count == BUFSIZE) {
-            UART0Count = 0; /* buffer overflow */
-        }
-    }
+	LSRValue = LPC_UART1->LSR;
 
-    if (IIRValue == IIR_THRE) /* THRE, transmit holding register empty */
-    {
-        /* THRE interrupt */
-        LSRValue = LPC_UART1->LSR; /* Check status in the LSR to see if
-                                                   valid data in U0THR or not */
-        if (LSRValue & LSR_THRE) {
-            UART1TxEmpty = 1;
-        } else {
-            UART1TxEmpty = 0;
-        }
-    }
+	if ( LSRValue & LSR_RDR )	/* Receive Data Ready */	
+	{
+		/* If no error on RLS, normal ready, save into the data buffer. */
+		/* Note: read RBR will clear the interrupt */
+		UART1Buffer[UART1Count] = LPC_UART1->RBR;
+		UART1Count++;
+		if ( UART1Count == BUFSIZE ){
+		UART0Count = 0;		/* buffer overflow */
+		}
+	}
+
+	if ( IIRValue == IIR_THRE )	/* THRE, transmit holding register empty */
+	{
+	/* THRE interrupt */
+		LSRValue = LPC_UART1->LSR;		/* Check status in the LSR to see if
+									valid data in U0THR or not */
+		if ( LSRValue & LSR_THRE ){
+			UART1TxEmpty = 1;
+		}
+		else{
+			UART1TxEmpty = 0;
+		}
+	}
+
 }
 
 /* By default, the PCLKSELx value is zero, thus, the PCLK for
-        all the peripherals is 1/4 of the SystemFrequency. */
-uint32_t getFrequency(uint32_t clk_slct) {
-    uint32_t pclk;
+	all the peripherals is 1/4 of the SystemFrequency. */
+uint32_t getFrequency(uint32_t clk_slct){
 
-    switch ((LPC_SC->PCLKSEL0 >> clk_slct) & 0x03) {
-        case 0x00:
-        default:
-            pclk = SystemCoreClock / 4;
-            break;
-        case 0x01:
-            pclk = SystemCoreClock;
-            break;
-        case 0x02:
-            pclk = SystemCoreClock / 2;
-            break;
-        case 0x03:
-            pclk = SystemCoreClock / 8;
-            break;
-    }
+	uint32_t pclk;
 
-    return pclk;
+	switch ( (LPC_SC->PCLKSEL0 >> clk_slct) & 0x03 )
+	{
+		case 0x00:
+		default:
+		pclk = SystemCoreClock/4;
+		break;
+		case 0x01:
+		pclk = SystemCoreClock;
+		break; 
+		case 0x02:
+		pclk = SystemCoreClock/2;
+		break; 
+		case 0x03:
+		pclk = SystemCoreClock/8;
+		break;
+	}
+
+	return pclk;
 }
 
 /*****************************************************************************
 ** Function name:		UARTInit
 **
 ** Descriptions:		Initialize UART port, setup pin select,
-**						clock, parity, stop bits, FIFO,
-*etc.
+**						clock, parity, stop bits, FIFO, etc.
 **
 ** parameters:			portNum(0 or 1) and UART baudrate
-** Returned value:		true or false, return false only if the
-**						interrupt handler can't be
-*installed to the *						VIC table
-**
+** Returned value:		true or false, return false only if the 
+**						interrupt handler can't be installed to the 
+**						VIC table
+** 
 *****************************************************************************/
-uint32_t UARTInit(uint32_t PortNum, uint32_t baudrate) {
-    uint32_t Fdiv;
-    uint32_t pclk;
+uint32_t UARTInit( uint32_t PortNum, uint32_t baudrate )
+{
+	uint32_t Fdiv;
+	uint32_t  pclk;
 
-    if (PortNum == 0) {
-        LPC_PINCON->PINSEL0 &= ~0x000000F0;
-        LPC_PINCON->PINSEL0 |= 0x00000050; /* RxD0 is P0.3 and TxD0 is P0.2 */
+	if ( PortNum == 0 )
+	{
+		LPC_PINCON->PINSEL0 &= ~0x000000F0;
+		LPC_PINCON->PINSEL0 |= 0x00000050;  /* RxD0 is P0.3 and TxD0 is P0.2 */
 
-        /* Bit 6~7 is for UART0 */
-        pclk = getFrequency(6);
+		/* Bit 6~7 is for UART0 */
+		pclk = getFrequency(6);
 
-        LPC_UART0->LCR = 0x83; /* 8 bits, no Parity, 1 Stop bit, The access to
-                                  Divisor latches is enabled. */
+		LPC_UART0->LCR = 0x83;		/* 8 bits, no Parity, 1 Stop bit, The access to Divisor latches is enabled. */
 
-        Fdiv = (pclk / 16) / baudrate; /*baud rate */
-        LPC_UART0->DLM = Fdiv / 256;
-        LPC_UART0->DLL = Fdiv % 256;
+		Fdiv = ( pclk / 16 ) / baudrate ;	/*baud rate */
+		LPC_UART0->DLM = Fdiv / 256;					
+		LPC_UART0->DLL = Fdiv % 256;
 
-        LPC_UART0->LCR = 0x03; /* DLAB = 0 */
-        LPC_UART0->FCR = 0x07; /* Enable and reset TX and RX FIFO. */
+		LPC_UART0->LCR = 0x03;		/* DLAB = 0 */
+		LPC_UART0->FCR = 0x07;		/* Enable and reset TX and RX FIFO. */
 
-        NVIC_EnableIRQ(UART0_IRQn);
+	 	NVIC_EnableIRQ(UART0_IRQn);
 
-        // LPC_UART0->IER = IER_RBR | IER_THRE | IER_RLS;	/* Enable UART0
-        // interrupt
-        // */
-        // LPC_UART0->IER =  IER_THRE ;//| IER_RLS;			/*
-        // Disable RBR
-        // */
+		//LPC_UART0->IER = IER_RBR | IER_THRE | IER_RLS;	/* Enable UART0 interrupt */
+		//LPC_UART0->IER =  IER_THRE ;//| IER_RLS;			/* Disable RBR */
 
-        FreeRcv(0);
-        FreeSnd(0);
-        return (TRUE);
-    } else if (PortNum == 1) {
-        LPC_PINCON->PINSEL4 &= ~0x0000000F;
-        LPC_PINCON->PINSEL4 |= 0x0000000A; /* Enable RxD1 P2.1, TxD1 P2.0 */
+		FreeRcv(0);
+		FreeSnd(0);
+		return (TRUE);
+	}
+	else if ( PortNum == 1 )
+	{
+		LPC_PINCON->PINSEL4 &= ~0x0000000F;
+		LPC_PINCON->PINSEL4 |= 0x0000000A;	/* Enable RxD1 P2.1, TxD1 P2.0 */
 
-        /* By default, the PCLKSELx value is zero, thus, the PCLK for
-        all the peripherals is 1/4 of the SystemFrequency. */
-        /* Bit 8,9 are for UART1 */
-        pclk = getFrequency(8);
+	/* By default, the PCLKSELx value is zero, thus, the PCLK for
+	all the peripherals is 1/4 of the SystemFrequency. */
+	/* Bit 8,9 are for UART1 */
+		pclk = getFrequency(8);
 
-        LPC_UART1->LCR = 0x83; /* 8 bits, no Parity, 1 Stop bit */
+		LPC_UART1->LCR = 0x83;		/* 8 bits, no Parity, 1 Stop bit */
 
-        Fdiv = (pclk / 16) / baudrate; /*baud rate */
-        LPC_UART1->DLM = Fdiv / 256;
-        LPC_UART1->DLL = Fdiv % 256;
+		Fdiv = ( pclk / 16 ) / baudrate ;	/*baud rate */
+		LPC_UART1->DLM = Fdiv / 256;					
+		LPC_UART1->DLL = Fdiv % 256;
 
-        LPC_UART1->LCR = 0x03; /* DLAB = 0 */
-        LPC_UART1->FCR = 0x07; /* Enable and reset TX and RX FIFO. */
+		LPC_UART1->LCR = 0x03;		/* DLAB = 0 */
+		LPC_UART1->FCR = 0x07;		/* Enable and reset TX and RX FIFO. */
 
-        NVIC_EnableIRQ(UART1_IRQn);
+	 	NVIC_EnableIRQ(UART1_IRQn);
 
-        // LPC_UART1->IER = IER_RBR | IER_THRE | IER_RLS;	/* Enable UART1
-        // interrupt
-        // */
+		//LPC_UART1->IER = IER_RBR | IER_THRE | IER_RLS;	/* Enable UART1 interrupt */
 
-        FreeRcv(1);
-        FreeSnd(1);
+		FreeRcv(1);
+		FreeSnd(1);
 
-        return (TRUE);
-    }
-    return (FALSE);
+		return (TRUE);
+	}
+	return( FALSE ); 
 }
 
 /*****************************************************************************
@@ -269,128 +282,130 @@ uint32_t UARTInit(uint32_t PortNum, uint32_t baudrate) {
 **
 ** parameters:			portNum, buffer pointer, and data length
 ** Returned value:		None
-**
+** 
 *****************************************************************************/
 
-void UARTSend(uint32_t portNum, uint8_t *BufferPtr, uint32_t Length) {
-    LPC_UART_TypeDef *LPC_UART;
-    volatile unsigned char *UARTTxEmpty;  // ASK Douglas
-    uint8_t *localBufferPtr;
-    uint32_t localLength;
+void UARTSend( uint32_t portNum, uint8_t *BufferPtr, uint32_t Length )
+{
+	LPC_UART_TypeDef *LPC_UART;
+	volatile unsigned char *UARTTxEmpty;				//ASK Douglas
+	uint8_t *localBufferPtr;
+	uint32_t localLength;
 
-    if ((portNum >> 1) != 0) return;
+	if((portNum >> 1 ) != 0)
+		return;
 
-    localLength = Length;
-    localBufferPtr = BufferPtr;
-    UARTTxEmpty = (portNum == 0 ? &UART0TxEmpty : &UART1TxEmpty);
-    LPC_UART = (portNum == 0 ? (LPC_UART_TypeDef *)LPC_UART0
-                             : (LPC_UART_TypeDef *)LPC_UART1);
+	localLength = Length;
+	localBufferPtr = BufferPtr;
+	UARTTxEmpty = (portNum == 0 ? &UART0TxEmpty : &UART1TxEmpty);
+	LPC_UART = (portNum == 0 ? (LPC_UART_TypeDef *)LPC_UART0 : (LPC_UART_TypeDef *)LPC_UART1 );
 
-    // Enable interupt
-    LPC_UART->IER |= IER_THRE;
+	//Enable interupt
+	LPC_UART->IER |=  IER_THRE;
 
-    while (LockSnd(portNum))
-        ;
+	while( LockSnd(portNum));
 
-    while (localLength != 0) {
-        /* THRE status, contain valid data */
-        while (!(*UARTTxEmpty & 0x01))
-            ;
-        LPC_UART->THR = *localBufferPtr;
-        *UARTTxEmpty = 0; /* not empty in the THR until it shifts out */
-        localBufferPtr++;
-        localLength--;
-    }
+	while ( localLength != 0 ){
+		/* THRE status, contain valid data */
+		while ( !(*UARTTxEmpty & 0x01) );
+		LPC_UART->THR = *localBufferPtr;
+		*UARTTxEmpty = 0;	/* not empty in the THR until it shifts out */
+		localBufferPtr++;
+		localLength--;
+	}
 
-    FreeSnd(portNum);
+	FreeSnd(portNum);
 
-    // Reanble other interpts
-    LPC_UART->IER &= ~IER_THRE;
+	//Reanble other interpts
+	LPC_UART->IER &= ~IER_THRE;
 
-    return;
+	return;
 }
 
-void UARTSendChar(uint32_t portNum, uint8_t character) {
-#ifdef __RTGT_UART
-    LPC_UART_TypeDef *LPC_UART;
-    LPC_UART = (portNum == 0 ? (LPC_UART_TypeDef *)LPC_UART0
-                             : (LPC_UART_TypeDef *)LPC_UART1);
-    while (!(LPC_UART->LSR & 0x20))
-        ;
-    LPC_UART->THR = character;
-#else
-    ITM_SendChar(character);
-#endif
+void UARTSendChar( uint32_t portNum, uint8_t character)
+{
+	#ifdef __RTGT_UART
+		LPC_UART_TypeDef *LPC_UART;
+		LPC_UART = (portNum == 0 ? (LPC_UART_TypeDef *)LPC_UART0 : (LPC_UART_TypeDef *)LPC_UART1 );
+		while (!(LPC_UART->LSR & 0x20));
+		LPC_UART->THR = character;
+	#else
+		ITM_SendChar(character);
+	#endif
+
 }
+
 
 /*****************************************************************************
 ** Function name:		UARTRecieve
 **
-** Descriptions:		Recieve a block of data to the UART 0-1 port
-*based *						on the data length
+** Descriptions:		Recieve a block of data to the UART 0-1 port based
+**						on the data length
 **
 ** parameters:			portNum, buffer pointer, and data length
 ** Returned value:		integer showing status
-**
+** 
 *****************************************************************************/
-uint32_t UARTRecieve(uint32_t portNum, uint8_t *BufferPtr, uint32_t Length) {
-    LPC_UART_TypeDef *LPC_UART;
-    volatile uint32_t *UARTCount;  // ASK Douglas
-    volatile uint8_t *UARTBuffer;
-    uint8_t *rcvdBufferPtr;
-    uint32_t rcvd_len, i;
+uint32_t UARTRecieve( uint32_t portNum, uint8_t *BufferPtr, uint32_t Length )
+{
 
-    if ((portNum >> 1) != 0) return 0;
+	LPC_UART_TypeDef *LPC_UART;
+	volatile uint32_t *UARTCount;				//ASK Douglas
+	volatile uint8_t *UARTBuffer;
+	uint8_t *rcvdBufferPtr;
+	uint32_t rcvd_len, i;
 
-    rcvd_len = 0x0;
-    rcvdBufferPtr = BufferPtr;
-    UARTCount = (portNum == 0 ? &UART0Count : &UART1Count);
-    UARTBuffer = (portNum == 0 ? UART0Buffer : UART1Buffer);
-    LPC_UART = (portNum == 0 ? (LPC_UART_TypeDef *)LPC_UART0
-                             : (LPC_UART_TypeDef *)LPC_UART1);
+	if((portNum >> 1 ) != 0)
+		return 0;
 
-    *UARTCount = 0x0;
+	rcvd_len = 0x0;
+	rcvdBufferPtr = BufferPtr;
+	UARTCount = (portNum == 0 ? &UART0Count : &UART1Count);
+	UARTBuffer = (portNum == 0 ? UART0Buffer : UART1Buffer);
+	LPC_UART = (portNum == 0 ? (LPC_UART_TypeDef *)LPC_UART0 : (LPC_UART_TypeDef *)LPC_UART1 );
 
-    // Enable interupt
-    LPC_UART->IER |= IER_RBR;
+	*UARTCount = 0x0;
 
-    // busy waiting
+	//Enable interupt
+	LPC_UART->IER |=  IER_RBR;
 
-    while (*UARTCount == 0)
-        ;
+	//busy waiting
 
-    // This part has to be put in the critical section
-    while (LockRcv(portNum))
-        ;
+	while( *UARTCount == 0 );
 
-    // Reanble other interpts
-    LPC_UART->IER &= ~IER_RBR; /* Disable RBR */
 
-    for (i = 0; i < *UARTCount; ++i) rcvdBufferPtr[rcvd_len++] = UARTBuffer[i];
+	//This part has to be put in the critical section
+	while(LockRcv(portNum));
 
-    *UARTCount = 0;
+	//Reanble other interpts
+	LPC_UART->IER &= ~IER_RBR;			/* Disable RBR */
 
-    FreeRcv(portNum);
 
-    return rcvd_len;
+	for(i = 0; i < *UARTCount; ++i)
+		rcvdBufferPtr[rcvd_len++] = UARTBuffer[i];
+
+	*UARTCount = 0;
+
+	FreeRcv(portNum);
+
+	return rcvd_len;
 }
 
-uint8_t UARTReceiveChar(uint32_t portNum) {
-#ifdef __RTGT_UART
-    /*uint8_t ret[1];
-    if (UARTRecieve(portNum, ret, 1) == 1)
-            return ret[0];
-    return 0x0;	*/
-    LPC_UART_TypeDef *LPC_UART;
-    LPC_UART = (portNum == 0 ? (LPC_UART_TypeDef *)LPC_UART0
-                             : (LPC_UART_TypeDef *)LPC_UART1);
-    while (!(LPC_UART->LSR & 0x01))
-        ;
-    return (LPC_UART->RBR);
-#else
-    while (ITM_CheckChar() != 1) __NOP();
-    return (ITM_ReceiveChar());
-#endif
+uint8_t UARTReceiveChar( uint32_t portNum)
+{
+	#ifdef __RTGT_UART
+		/*uint8_t ret[1];
+		if (UARTRecieve(portNum, ret, 1) == 1)
+			return ret[0];
+		return 0x0;	*/
+		LPC_UART_TypeDef *LPC_UART;
+		LPC_UART = (portNum == 0 ? (LPC_UART_TypeDef *)LPC_UART0 : (LPC_UART_TypeDef *)LPC_UART1 );
+		while (!(LPC_UART->LSR & 0x01));
+		return (LPC_UART->RBR);
+	#else
+		while (ITM_CheckChar() != 1) __NOP();
+		return (ITM_ReceiveChar());
+	#endif
 }
 
 /******************************************************************************
